56cdbe6b094af350277983508fe5bd6676d13a35
erick_wendel <erick.workspace@gmail.com>
2015-12-05 02:31:28 +0000

    Correcao de injecao de servico

--- www/js/services/clientService.js
+++ www/js/services/clientService.js
@@ -2,7 +2,7 @@
 	'use strict';
 	var app = angular.module('services.clientService', []);
 	
-	app.service('clientService', ['$http', 'ConstantsService', function ($http, ConstantsService) {
+	app.service('ClientService', ['$http', 'ConstantsService', function ($http, ConstantsService) {
 		return {
 			Create : function (user) {
 				$http.post(ConstantsService.CREATE_USER_URL, user).then(function (e) {
