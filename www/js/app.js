98d692732c46e65c5326086dc00ba5f062a2a010
erick.workspace <erick.workspace@hotmail.com>
2016-01-26 16:48:40 -0200

    correçoes de arquivos, dependencias e projeto inicial

--- www/js/app.js
+++ www/js/app.js
@@ -3,22 +3,94 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('artmanager', [
+  'ionic', 
+  'controllers.appController',
+  'controllers.loginController',
+  'controllers.dashboardController',
+  'controllers.userController',
+  'controllers.providerController',
+  'controllers.clientController',
+  'services.loginService',
+  'services.utilService',
+  'services.userService',
+  'services.clientService',
+  'ionic-material', 
+  'ionMdInput',
+  'ui.utils.masks'
+  ]);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+  if(window.cordova && window.cordova.plugins.Keyboard) {
+    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  }
+  if(window.StatusBar) {
+    StatusBar.styleDefault();
+  }
+});
+});
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+app.config(function ($urlRouterProvider, $stateProvider, $ionicConfigProvider) {
+  $ionicConfigProvider.views.maxCache(0);
+
+  var stateProvider =  $stateProvider;
+
+  stateProvider.state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  });
+
+  stateProvider.state('app.login', {
+    url: '/login',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      }
     }
   });
-})
+  stateProvider.state('app.dashboard', {
+    url: '/dashboard',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/dashboard.html',
+        controller: 'DashboardCtrl'
+      }
+    }
+  });  
+  stateProvider.state('app.createUser', {
+    url: '/createUser',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/form-user.html',
+        controller: 'UserCtrl'
+      }
+    }
+  });
+  stateProvider.state('app.createClient', {
+    url: '/createClient',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/form-client.html',
+        controller: 'ClientCtrl'
+      }
+    }
+  });
+
+  stateProvider.state('app.createProvider', {
+    url: '/createProvider',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/form-provider.html',
+        controller: 'ProviderCtrl'
+      }
+    }
+  });
+    
+  $urlRouterProvider.otherwise('/app/login');
+});
