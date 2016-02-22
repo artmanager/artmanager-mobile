d64048393d1ce0f2c96626735a4b863083ff4f67
erick.workspace <erick.workspace@hotmail.com>
2016-02-21 21:23:33 -0300

    Refactoring in login

--- www/app/login/controllers/loginController.js
+++ www/app/login/controllers/loginController.js
@@ -2,20 +2,19 @@
     'user strict';
     var app = angular.module('controllers.loginController', []);
 
-    app.controller('LoginCtrl', ['$scope', '$state', '$timeout', '$stateParams', 'LoginService', 'LocalStorageService', 'UtilService',
-        function ($scope, $state, $timeout, $stateParams, LoginService, LocalStorageService, UtilService) {
+    app.controller('LoginCtrl', ['$state', 'LoginService', 'LocalStorageService', 'UtilService',
+        function ($state, LoginService, LocalStorageService, UtilService) {
+            var vm  = this;
 
-
-            $scope.user = { 'name': '', 'password': '' };
-
-            $scope.login = function (user) {
-                var data = { 'data': btoa(user.name + "-" + user.password) };
+            vm.user = { 'name': '', 'password': '' };
+            
+            vm.login = function (user) {
+                var data = { 'data': btoa(vm.user.name + "-" + vm.user.password) };
                 
                 // LoginService.login(data).then(function (token) {
                 //     if(token != null){
                 //         LocalStorageService.set('token',token);
-                // var a = document.querySelector('body > ion-nav-view > div > link')
-                // a.parentNode.removeChild(a)
+                
                 UtilService.removeCSS("app/login/login.css");
                 $state.go('app.orders');
                 //     }
