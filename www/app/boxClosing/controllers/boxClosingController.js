a1fc24c6b916c67a487a5d45ee77c82c6a1e0c08
erick.workspace <erick.workspace@hotmail.com>
2016-05-27 21:57:33 -0300

    finished boxClosing

--- www/app/boxClosing/controllers/boxClosingController.js
+++ www/app/boxClosing/controllers/boxClosingController.js
@@ -1,10 +1,96 @@
 (function (angular) {
     angular.module('controllers.boxClosingController', [])
         .controller('BoxClosingCtrl', BoxClosingCtrl);
-    BoxClosingCtrl.$inject = [];
+    BoxClosingCtrl.$inject = ['BoxClosingService', 'toastr', 'LoadingPopup'];
 
-    function BoxClosingCtrl() {
+    function BoxClosingCtrl(BoxClosingService, toastr, LoadingPopup) {
         var vm = this;
-        
+        vm.items = [];
+        vm.itemsFilter = [];
+        vm.initialDate = null;
+        vm.finalDate = null;
+        vm.search = search;
+        vm.filters = getFilters();
+        vm.dateRef = getDateRef;
+
+        (function init() {
+            loadBoxItens();
+        })();
+        function loadBoxItens() {
+            console.log('carregou');
+            LoadingPopup.show();
+            BoxClosingService.get().then(onGetSuccess, onGetError);
+        }
+
+        function getDateRef(month, year) {
+            month = month >= 10 ? month : "0" + month;
+            return month + "/" + year;
+        }
+        function getFilters() {
+            var filters = [];
+            var filter = {};
+
+            filter = {};
+            filter.value = "date";
+            filter.desc = "Data";
+            filters.push(filter);
+
+
+            filter = {};
+            filter.value = "total";
+            filter.desc = "Total";
+            filters.push(filter);
+
+            filter = {};
+            filter.value = "totalCommission";
+            filter.desc = "Comissoes";
+            filters.push(filter);
+            
+            filter = {};
+            filter.value = "totalSaleProducts";
+            filter.desc = "Vendas";
+            filters.push(filter);
+            
+            filter = {};
+            filter.value = "totalProductionProducts";
+            filter.desc = "Produção";
+            filters.push(filter);
+            
+            filter = {};
+            filter.value = "totalProductionPending";
+            filter.desc = "Pendencias";
+            filters.push(filter);
+            
+            return filters;
+        }
+        function search() {
+            var itens = angular.copy(vm.itemsFilter);
+
+            var initialDate = vm.initialDate || new Date(0);
+            var finalDate = vm.finalDate || new Date();
+
+            var filteredItens = itens.filter(function (item) {
+                var dateItem = new Date(item.year, (item.month), 01);
+                return dateItem > initialDate && dateItem < finalDate;
+
+            });
+            vm.items = filteredItens;
+
+        }
+        function onGetSuccess(result) {
+            LoadingPopup.hide();
+            var itens = result.success.map(function (item) {
+                item.date = getDateRef(item.month, item.year);
+                return item;
+            });
+            vm.items = itens;
+            vm.itemsFilter = itens;
+        }
+
+
+        function onGetError(result) {
+            LoadingPopup.hide();
+            toastr.error('Não foi possivel carregar os itens');
+        }
     }
 })(angular);
--- www/app/boxClosing/services/boxClosingService.js
+++ www/app/boxClosing/services/boxClosingService.js
@@ -3,9 +3,40 @@
 
     app.service('BoxClosingService', ['$q', '$http', 'ConstantsService', 'AuthService', function ($q, $http, ConstantsService, AuthService) {
         return {
-            employess: employess,
-            employee: getDetail
+            get: getItens
         };
+
+        function getItens() {
+            // return $http({
+            //     method: 'GET',
+            //     url: ConstantsService.GET_PRODUCTION_URL,
+            //     headers: AuthService.headers()
+            // }).then(function (params) {
+            //     return params.data;
+            // });
+            var itens = [{
+                "total": 10.000,
+                "totalCommission": 10.000,
+                "totalSaleProducts": 100,
+                "totalProductionProducts": 50,
+                "totalProductionPending": 60,
+                "year" : 2016,
+                "month": 4
+            }, 
+            {
+                "total": 5.000,
+                "totalCommission": 5.000,
+                "totalSaleProducts": 50,
+                "totalProductionProducts": 10,
+                "totalProductionPending": 70,
+                "year" : 2016,
+                "month": 3
+            }];
+
+            var defer = $q.defer();
+            defer.resolve({success : itens});
+            return defer.promise;
+        }
     }
     ]);
 })(angular);
