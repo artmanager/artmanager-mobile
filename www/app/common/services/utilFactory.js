4eb973ff6e9a68312ea6db3be29cd469a8af38f3
erick.workspace <erick.workspace@hotmail.com>
2016-05-28 04:38:46 -0300

    corrections in loading popup

--- www/app/common/services/utilFactory.js
+++ www/app/common/services/utilFactory.js
@@ -12,31 +12,31 @@
         var baseClient = BASE_API_URL + 'client';
         var baseProvider = BASE_API_URL + 'supplier';
         var baseProduct = BASE_API_URL + 'product';
-        var baseOrder= BASE_API_URL + 'which';
+        var baseOrder = BASE_API_URL + 'which';
         var baseProduction = BASE_API_URL + 'production';
-        
+
         this.LOGIN_URL = baseAutenticacao;
         this.CREATE_USER_URL = baseUser;
-        
+
         this.CREATE_CLIENT_URL = baseClient;
         this.GET_CLIENT_URL = baseClient;
-        
+
         this.CREATE_PROVIDER_URL = baseProvider;
         this.GET_PROVIDER_URL = baseProvider;
 
         this.CREATE_PRODUCT_URL = baseProduct;
         this.GET_PRODUCT_URL = baseProduct;
-        
+
         this.CREATE_ORDER_URL = baseOrder;
         this.GET_ORDER_URL = baseOrder;
         this.ACCESS_TOKEN_KEY = 'x-access-token';
 
         this.GET_PRODUCTION_URL = baseProduction;
         this.UPDATE_STATUS_PRODUCTION = baseProduction;
-        
+
         this.UPDATE_PRODUCTION_STATUS = BASE_API_URL + 'updateEntrancePending';
-        this.REPORT_PRODUCTS  = BASE_API_URL + 'reportProduct';
-        
+        this.REPORT_PRODUCTS = BASE_API_URL + 'reportProduct';
+
         this.GET_CATEGORIES_PRODUCTS = BASE_API_URL + 'productCategory';
         this.CREATE_CATEGORIES_PRODUCTS = BASE_API_URL + 'productCategory';
     }
@@ -61,53 +61,58 @@
         return {
             removeCSS: removeCSS
         };
-        
+
         function removeCSS(cssPath) {
             var node = document.querySelector("[href='" + cssPath + "']");
             node.parentNode.removeChild(node);
         }
     }
     app.factory('LoadingPopup', LoadingPopup);
-    LoadingPopup.$inject = ['$ionicLoading'];
-    function LoadingPopup($ionicLoading) {
+    LoadingPopup.$inject = ['$ionicLoading', '$rootScope'];
+    function LoadingPopup($ionicLoading, $rootScope) {
         return {
             show: show,
             hide: hide
         };
-        
+
         function hide() {
             $ionicLoading.hide();
         }
-        
+
         function show() {
+            $rootScope.cancel = $ionicLoading.hide;
             $ionicLoading.show({
-                template: '<md-progress-circular md-mode="indeterminate" md-diameter="96"></md-progress-circular>',
-                showBackdrop: false,
+                template: '<button class="button button-clear" ' + 
+                           'style="line-height: normal; min-height: 0; min-width: 0;"' + 
+                           'ng-click="cancel()">' + 
+                           '<i class="ion-close-circled"></i>' + 
+                           '</button> Carregando...',
+                showBackdrop: true,
                 hideOnStateChange: true,
                 animation: 'fade-in',
-
+                scope: $rootScope
             });
-            
+
         }
     }
-    
+
     app.factory('AuthService', AuthService);
     function AuthService() {
         return {
-          headers: headers  
+            headers: headers
         };
         function headers() {
             return { 'x-access-token': localStorage.token };
-        } 
+        }
     }
-    
+
     app.factory('DateService', DateService);
     function DateService() {
-        return { 
-            daysBetween : daysBetween,
-            formatToLocaleDate : formatToLocaleDate 
+        return {
+            daysBetween: daysBetween,
+            formatToLocaleDate: formatToLocaleDate
         };
-        
+
         function treatAsUTC(date) {
             var result = new Date(date);
             result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
@@ -121,9 +126,9 @@
         function formatToLocaleDate(params) {
             var month = (params.getMonth() + 1);
             var formatedMonth = month >= 10 ? month : "0" + month;
-            var date = params.getDate() + "/" + formatedMonth + "/"+ params.getFullYear();
-            
-            return date;   
+            var date = params.getDate() + "/" + formatedMonth + "/" + params.getFullYear();
+
+            return date;
         }
     }
     //  app.factory('httpRequestInterceptor', httpRequestInterceptor);
